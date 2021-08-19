724404b51764e9f9edb258bc363c4aa034b766c8
HimanshuChittora23908 <2020kucp1023@iiitkota.ac.in>
2021-08-19 18:58:32 +0530

    Landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from 'next'
+import Image from "next/image"
+import isometric_image from "../images/Isometric_Image.png"
 
 const Home: NextPage = () => {
   return (
     <>
-    <div>
-      <h1 className="m-8 text-2xl">Hello World!</h1>
+    <div className="bg-gray-900 bg-contain flex justify-between">
+      <div>
+        <h1 className="text-white">Hello World</h1>
+      </div>
+      <div className="m-32 w-2/5 h-2/5">
+        <Image src={isometric_image} alt="Isometric_Image"  />
+      </div>
     </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          1000 : "0B0B0B",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
