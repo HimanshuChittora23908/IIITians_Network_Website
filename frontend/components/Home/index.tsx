731d33d044e6c2999f9f1e85d655b36e2144fc75
SHIVSB <75856462+SHIVSB@users.noreply.github.com>
2021-08-23 15:29:31 +0530

    Update index.tsx
    Footer

--- frontend/components/Home/index.tsx
+++ frontend/components/Home/index.tsx
@@ -3,4 +3,5 @@ import Thinking from "./Thinking";
 import WorkingFields from "./WorkingFields";
 import StartPage from "./StartPage";
 import Statistics from "./Statistics";
-export { Performance, Statistics, WorkingFields, Thinking, StartPage };
+import Footer from "./Footer"
+export { Performance, Statistics, WorkingFields, Thinking, StartPage, Footer};
