05d11429cbae830cd10b0b3f5c5d6601e71a81e8
RaghhavDTurki <raghhavdturki@gmail.com>
2021-09-09 19:49:17 +0530

    Update OAuth config

--- server/src/config/OAuthProvider/github.ts
+++ server/src/config/OAuthProvider/github.ts
@@ -1,24 +1,53 @@
+import mongoose from "mongoose";
 import passport from "passport"
-import { Strategy } from "passport-github";
+import { Profile, Strategy } from "passport-github";
+import User, { UserClass } from "../../entity/User";
 
-passport.serializeUser((user: Express.User, done) => {
-    done(null, user);
-})
-
-passport.deserializeUser((user: Express.User, done) => {
-    done(null, user);
-})
-
-export default function passportGithubConfig(): void{
-    passport.use(new Strategy({
-        clientID: process.env.CLIENT_ID_GITHUB as string,
-        clientSecret: process.env.CLIENT_SECRET_GITHUB as string,
-        callbackURL: "http://localhost:4000/auth/github/callback",
-        // passReqToCallback: true
-      },
-      function(accessToken, refreshToken, profile, cb) {
-          console.log(accessToken, profile)
-          return cb(null, profile)
+const insertUser = async (provider: string, profile: Profile, done: (error: any, user?: any, info?: any) => void
+): Promise<void> => {
+  try{
+      const email = (profile._json as any).email;
+    const user = await User.findOne({ email });
+    
+      if(user){
+          return done(null, user);
       }
-    ));
+  
+      const newUser = await User.create({
+          email,
+          username: profile.displayName || profile.username,
+          profileId: profile.profileUrl,
+          provider,
+          socialId: profile.id,
+          confirmed: true
+      });
+      console.log(provider);
+      done(null, newUser);
+    }
+    catch(e){
+      done(e, null);
+    }
 }
+
+
+const passportGithubConfig = new Strategy({
+    clientID: process.env.CLIENT_ID_GITHUB as string,
+    clientSecret: process.env.CLIENT_SECRET_GITHUB as string,
+    callbackURL: "http://localhost:4000/auth/github/callback",
+    // passReqToCallback: true
+  },
+  async(accessToken, refreshToken, profile, done) => {
+    await insertUser(profile.provider, profile, done);
+});
+
+passport.serializeUser((user: UserClass, done) => {
+    return done(null, user._id);
+  });
+  
+passport.deserializeUser(async (id: string, done) => {
+    if (!mongoose.isValidObjectId(id)) return done(null, null);
+    const user = await User.findById(id);
+    return done(null, user);
+});
+
+passport.use(passportGithubConfig)
