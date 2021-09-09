f88b4a34811d156807b77dcf6c1b336d37a2fa4c
RaghhavDTurki <raghhavdturki@gmail.com>
2021-09-09 19:50:00 +0530

    Update OAuth config

--- server/src/config/OAuthProvider/google.ts
+++ server/src/config/OAuthProvider/google.ts
@@ -1,25 +1,54 @@
+import User, { UserClass } from '../../entity/User';
 import passport from "passport";
-import { Strategy } from "passport-google-oauth20";
+import { Profile, Strategy } from "passport-google-oauth20";
+import mongoose from 'mongoose';
 
-passport.serializeUser((user: Express.User, done) => {
-    done(null, user);
-})
-
-passport.deserializeUser((user: Express.User, done) => {
-    done(null, user);
-})
-
-
-export default function googlePassportConfig(): void{
-    passport.use(new Strategy({
-        clientID: process.env.CLIENT_ID_GOOGLE as string,
-        clientSecret: process.env.CLIENT_SECRET_GOOGLE as string,
-        callbackURL: "http://localhost:4000/auth/google/callback",
-      },
-      function(accessToken, refreshToken,profile, cb) {
-        console.log(accessToken,profile);
-        return cb(null, profile);
-      }));
+const insertUser = async (provider: string, profile: Profile, done: (error: any, user?: any, info?: any) => void
+): Promise<void> => {
+  try{
+    const email = profile.emails![0].value;
+    const user = await User.findOne({ email });
+    
+      if(user){
+          return done(null, user);
+      }
+  
+      const newUser = await User.create({
+          email,
+          username: profile.displayName || profile.username,
+          provider,
+          socialId: profile.id,
+          confirmed: true
+      });
+      console.log(provider);
+      done(null, newUser);
+    }
+    catch(e){
+      done(e, null);
+    }
 }
 
 
+const googlePassportConfig = new Strategy(
+  {
+    clientID: process.env.CLIENT_ID_GOOGLE as string,
+    clientSecret: process.env.CLIENT_SECRET_GOOGLE as string,
+    callbackURL: "http://localhost:4000/auth/google/callback",
+  },
+  async (accessToken, refreshToken, profile , done) => {
+    // console.log(accessToken, profile)
+    await insertUser(profile.provider, profile, done);
+  }
+);
+
+passport.serializeUser((user: UserClass, done) => {
+  return done(null, user._id);
+});
+
+passport.deserializeUser(async (id: string, done) => {
+  if (!mongoose.isValidObjectId(id)) return done(null, null);
+  const user = await User.findById(id);
+  return done(null, user);
+});
+
+passport.use(googlePassportConfig);
