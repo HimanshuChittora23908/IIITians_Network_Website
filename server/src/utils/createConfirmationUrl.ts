41d7273e37968b50410bba3ad13b285bfcf30ad7
RaghhavDTurki <raghhavdturki@gmail.com>
2021-09-10 19:26:33 +0530

    Update Confirmation Url

--- server/src/utils/createConfirmationUrl.ts
+++ server/src/utils/createConfirmationUrl.ts
@@ -1,9 +1,10 @@
 import { v4 } from "uuid";
 import { redis } from "../config/redis";
+import { confirmUserPrefix } from "./constants/RedisPrefix";
 
 export const createConfirmationUrl = async (userId: string): Promise<string> => {
   const token = v4();
-  await redis.set(token, userId, "ex", 60 * 60 * 24); // 1 day expiration
+  await redis.set(confirmUserPrefix + token, userId, "ex", 60 * 60 * 24); // 1 day expiration
 
   return `http://localhost:3000/user/confirm/${token}`;
 };
