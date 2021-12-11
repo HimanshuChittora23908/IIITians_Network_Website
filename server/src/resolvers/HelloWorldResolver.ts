6acb6b17847284306b799f9ef185af74702ec4ea
RaghhavDTurki <raghhavdturki@gmail.com>
2021-12-11 10:14:56 +0530

    Testing graphql sessions again

--- server/src/resolvers/HelloWorldResolver.ts
+++ server/src/resolvers/HelloWorldResolver.ts
@@ -1,7 +1,8 @@
 import User ,{ UserClass } from './../entity/User';
-import { Query, Resolver, Authorized } from 'type-graphql';
+import { Query, Resolver, Authorized, Ctx } from 'type-graphql';
 import { DocumentType } from '@typegoose/typegoose';
 import { BeAnObject } from '@typegoose/typegoose/lib/types';
+import { Context } from '../types/Context';
 
 @Resolver()
 export class HelloWorldResolver {
@@ -15,5 +16,12 @@ export class HelloWorldResolver {
   async users(): Promise<DocumentType<UserClass, BeAnObject>[] | null> {
     const users = await User.find({});
     return users;
+  }
+
+  @Query(() => UserClass, { nullable : true })
+  async currentUser( @Ctx() ctx: Context ):Promise<DocumentType<UserClass, BeAnObject> | null>{
+      const uid = ctx.req.session.passport!.user;
+      const currentUser = await User.findById(uid);
+      return currentUser;
   }
 }
